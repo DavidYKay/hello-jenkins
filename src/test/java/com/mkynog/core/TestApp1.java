6547acb98656c9dcee4d922f562f93a578d4a66a
David Young-Chan Kay <davidykay@gmail.com>
2013-04-18 15:04:06 -0400

    created JUNit test project.

---
+++ src/test/java/com/mkynog/core/TestApp1.java
@@ -0,0 +1,21 @@
+package com.mkyong.core;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class TestApp1 {
+
+	@Test
+	public void testPrintHelloWorld() {
+
+		Assert.assertEquals(App.getHelloWorld(), "Hello World");
+
+	}
+
+        @Test
+	public void testNumberEquality() {
+		Assert.assertEquals(1, 3);
+        }
+
+
+}
